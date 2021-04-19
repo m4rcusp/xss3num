56f1351be364349cf16f2ac0f0a60c5c160015a3
m4rcusp <67711917+m4rcusp@users.noreply.github.com>
2021-04-18 22:06:50 -0300

    Update xss3enum.sh

--- xss3enum.sh
+++ xss3enum.sh
@@ -2,23 +2,23 @@
 
 . ~/.profile
 
-#echo "Insert a target:"
-#read host
-#echo "Insert your bug bounty program name and your username:"
-#echo "ex: hackerone:h4cker"
-#read username
-#echo "Looking for subdomains..."
-#subfinder -d $host | anew subf
-#echo "$host" | haktrails subdomains | anew haktrails1
-#assetfinder $host | anew assetf
-#/root/bounty/subdom/findomain -t $host -q | anew findomain1
-#amass enum -d $host | anew amass1
-#echo "Deleting duplicates..."
-#cat * | anew domains
-#echo "Done!"
-#echo "Validating domains..."
-#httpx -l domains -H "$username" -mc 200 -timeout 3 -silent -threads 1000 | anew httpx1
-#echo "Done!"
+echo "Insert a target:"
+read host
+echo "Insert your bug bounty program name and your username:"
+echo "ex: hackerone:h4cker"
+read username
+echo "Looking for subdomains..."
+subfinder -d $host | anew subf
+echo "$host" | haktrails subdomains | anew haktrails1
+assetfinder $host | anew assetf
+/root/bounty/subdom/findomain -t $host -q | anew findomain1
+amass enum -d $host | anew amass1
+echo "Deleting duplicates..."
+cat * | anew domains
+echo "Done!"
+echo "Validating domains..."
+httpx -l domains -H "$username" -mc 200 -timeout 3 -silent -threads 1000 | anew httpx1
+echo "Done!"
 echo "Looking for parameters..."
 rush -i httpx1 'python3 /root/bounty/subdom/ParamSpider/paramspider.py -d {}'
 cat output/http:/* output/https:/* | anew xss
